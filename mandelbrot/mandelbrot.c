a8b08c6afd12a5d4fdd865731b2a18fcdc212978
Lukas Bjarre <lbjarre@kth.se>
2018-02-04 11:01:51 +0100

    Added color calculation for entire grid

--- mandelbrot/mandelbrot.c
+++ mandelbrot/mandelbrot.c
@@ -3,14 +3,17 @@
 #include <complex.h>
 
 #define MAXITER 255
-#define ESCAPE 2
+
+#define W 256
+#define H 256
+#define B 2
 
 int mandelbrot(complex double c)
 {    
     int i = 0;
     complex double x = c;
 
-    while (cabs(x) < ESCAPE && i < MAXITER) {
+    while (cabs(x) < B && i < MAXITER) {
         x = x*x + c;
         ++i;
     }
@@ -18,9 +21,32 @@ int mandelbrot(complex double c)
     return i;
 }
 
+void mandelbrot_grid(int *M)
+{
+    double x, y;
+    complex double c;
+    int color;
+
+    double dx = 2*B / (W - 1);
+    double dy = 2*B / (H - 1);
+
+    for (int i = 0; i < H; ++i) {
+        x = i * dx - B;
+        for (int j = 0; j < W; ++j) {
+            y = j * dy - B;
+            c = x + y * I;
+            M[i + j*H] = mandelbrot(c);
+        }
+    }
+    
+}
+
 int main()
 {
-    complex double c = 0.1 - 1*I;
-    int i = mandelbrot(c);
-    printf("%d\n", i);
+    int M[W*H];
+    mandelbrot_grid(M);
+    for (int i = 0; i < W*H; ++i) {
+        printf("%d\n", M[i]);
+    }
+    return 0;
 }
