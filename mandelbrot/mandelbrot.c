d69e6a29198f4b628df770a5266e520b45578caa
Lukas Bjarre <lbjarre@kth.se>
2018-02-05 16:59:53 +0100

    Fixed loop variable initialization not working for non-C99

--- mandelbrot/mandelbrot.c
+++ mandelbrot/mandelbrot.c
@@ -25,15 +25,16 @@ int mandelbrot(complex double c)
 
 void mandelbrot_grid(int *M, int y_offset, int p_height)
 {
+    int i, j;
     double x, y;
     complex double c;
 
     double dx = 2*B/(W - 1);
     double dy = 2*B/(H - 1); 
 
-    for (int j = 0; j < p_height; ++j) {
+    for (j = 0; j < p_height; ++j) {
         y = (j + y_offset) * dy - B;
-        for (int i = 0; i < W; ++i) {
+        for (i = 0; i < W; ++i) {
             x = i * dx - B;
             c = x + y * I;
             M[j*W + i] = mandelbrot(c);
@@ -43,9 +44,10 @@ void mandelbrot_grid(int *M, int y_offset, int p_height)
 
 void write_to_file(int *M)
 {
+    int i, j;
     FILE *fp = fopen("color.txt", "w");
-    for (int j = 0; j < H; ++j) {
-        for (int i = 0; i < W; ++i) {
+    for (j = 0; j < H; ++j) {
+        for (i = 0; i < W; ++i) {
             fprintf(fp, "%hhu ", M[i + j*W]);
         }
         fprintf(fp, "\n");
@@ -55,7 +57,7 @@ void write_to_file(int *M)
 
 int main(int argc, char **argv)
 {
-    int rank, size, y_offset, p_h;
+    int rank, size, y_offset, p_h, i;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -74,10 +76,10 @@ int main(int argc, char **argv)
     if (rank == 0) {
         int full_array[H*W];
         int *p = full_array;
-        for (int i = 0; i < W*p_h; ++i) {
+        for (i = 0; i < W*p_h; ++i) {
             *p++ = M[i];
         }
-        for (int i = 1; i < size; ++i) {
+        for (i = 1; i < size; ++i) {
             MPI_Recv(p, W*p_h, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
             p += W*p_h;
         }
