2666bb4a7ea0dcfcde344a1cc2c4f7825b2b1abc
Lukas Bjarre <lbjarre@kth.se>
2018-02-07 20:01:19 +0100

    Various refactoring and feature adding
     - Improved memory management and efficiency for several parts of the program
     - Added error handeling for starting MPI with an incorrect ammount of processes
     - Added argument passing to main to set the viewport of the complex plane instead of defines

--- mandelbrot/mandelbrot.c
+++ mandelbrot/mandelbrot.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <complex.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 #define NUMB_COLORS 255
-#define ESCAPE_DIST 2.0
+#define ESC_DIST_SQ 4.0
+#define PIXEL_W  2048
+#define PIXEL_H  2048
 
-#define PIXEL_W  1024
-#define PIXEL_H  1024
-#define X_START -2.0
-#define X_W      4.0
-#define Y_START  2.0
-#define Y_H      4.0
-
-
-int mandelbrot(complex double c)
+int mandelbrot(double c_real, double c_imag)
 {    
     int i = 0;
-    complex double x = c;
+    double z_real = c_real;
+    double z_real_sq = c_real * c_real;
+    double z_imag = c_imag;
+    double z_imag_sq = c_imag * c_imag;
 
-    while (cabs(x) < ESCAPE_DIST && i < NUMB_COLORS) {
-        x = x*x + c;
+    while (z_real_sq + z_imag_sq < ESC_DIST_SQ && i < NUMB_COLORS) {
+        z_imag = 2 * z_real * z_imag + c_imag;
+        z_real = z_real_sq - z_imag_sq + c_real;
+        z_real_sq = z_real * z_real;
+        z_imag_sq = z_imag * z_imag;
         ++i;
     }
     
     return i;
 }
 
-void mandelbrot_grid(int *M, int y_offset, int process_h)
+void mandelbrot_grid(int *M, int y_offset, int process_height,
+                     double x_start, double y_start, double h_w)
 {
     int i, j;
     double x, y;
-    complex double c;
 
-    double dx = X_W/(PIXEL_W - 1);
-    double dy = -Y_H/(PIXEL_H - 1); 
+    double dx = h_w/(PIXEL_W - 1);
+    double dy = -h_w/(PIXEL_H - 1); 
 
-    for (j = 0; j < process_h; ++j) {
-        y = (j + y_offset) * dy + Y_START;
+    for (j = 0; j < process_height; ++j) {
+        y = (j + y_offset) * dy + y_start;
         for (i = 0; i < PIXEL_W; ++i) {
-            x = i * dx + X_START;
-            c = x + y * I;
-            *M++ = mandelbrot(c);
+            x = i * dx + x_start;
+            *M++ = mandelbrot(x, y);
         }
     }
 }
@@ -52,36 +49,72 @@ void write_to_file(FILE *fp, int *M, int process_height)
     int i, j;
     for (j = 0; j < process_height; ++j) {
         for (i = 0; i < PIXEL_W; ++i) {
-            fprintf(fp, "%hhu ", M[i + j*PIXEL_W]);
+            fprintf(fp, "%hhu ", *M++);
         }
         fprintf(fp, "\n");
     }
 }
 
-int main(int argc, char **argv)
+int init_mpi(int *size, int *rank, int argc, char **argv)
 {
-    int rank, size, y_offset, p_h, i;
-    MPI_Status status;
+    int flag = 0;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, size);
+    MPI_Comm_rank(MPI_COMM_WORLD, rank);
 
-    p_h = PIXEL_H / size;
-    y_offset = rank * p_h;
+    if (PIXEL_H % *size) {
+        flag = -1;
+    }
+    
+    return flag;
+}
 
-    int M[PIXEL_W*p_h];
-    mandelbrot_grid(M, y_offset, p_h);
-    if (rank != 0) {
-        MPI_Send(M, PIXEL_H*p_h, MPI_INT, 0, 0, MPI_COMM_WORLD);
+int set_viewport(double *x_start, double *y_start, double *h_w,
+                  int argc, char **argv)
+{
+    int flag = 0;
+    *x_start = -2.0;
+    *y_start = 2.0;
+    *h_w = 4.0;
+    if (argc == 4) {
+        sscanf(argv[1], "%lf", x_start);
+        sscanf(argv[2], "%lf", y_start);
+        sscanf(argv[3], "%lf", h_w);
+    }
+    else if (argc != 1) {
+        flag = -1;
+    }
+    return flag;
+}
+
+int main(int argc, char **argv)
+{
+    int rank, size, y_off, process_h, vec_len, i, flag_mpi, flag_arg;
+    double x_start, y_start, h_w;
+    MPI_Status st;
+
+    flag_arg = set_viewport(&x_start, &y_start, &h_w, argc, argv);
+    flag_mpi = init_mpi(&size, &rank, argc, argv);
+    if (flag_arg != 0 || flag_mpi != 0) {
+        exit(1);
     }
 
-    if (rank == 0) {
+    process_h = PIXEL_H / size;
+    vec_len = PIXEL_W * process_h;
+    y_off = rank * process_h;
+    int M[vec_len];
+    mandelbrot_grid(M, y_off, process_h, x_start, y_start, h_w);
+
+    if (rank != 0) {
+        MPI_Send(M, vec_len, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    else {
         FILE *fp = fopen("color.txt", "w");
-        write_to_file(fp, M, p_h);
+        write_to_file(fp, M, process_h);
         for (i = 1; i < size; ++i) {
-            MPI_Recv(M, PIXEL_H*p_h, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
-            write_to_file(fp, M, p_h);
+            MPI_Recv(M, vec_len, MPI_INT, i, 0, MPI_COMM_WORLD, &st);
+            write_to_file(fp, M, process_h);
         }
         fclose(fp);
     }
